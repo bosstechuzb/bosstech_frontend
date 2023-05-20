4da24dcbb202a68162a0f65bb592bfc0c7c28241
topilovasilbek <asilbektback@gmail.com>
2023-05-20 13:26:25 +0500

    scroll to top button added

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -4,12 +4,14 @@ import { GlobalStyles } from "./style";
 import Home from "./../pages/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import GetLang from "../resources/getLang.js";
+import ToTopButton from "../components/ToTopButton";
 
 const Root = () => {
   return (
     <>
       <Helmet />
       <GlobalStyles />
+      <ToTopButton />
       <GetLang />
       <Routes>
         <Route path="/home" element={<Home />} />
